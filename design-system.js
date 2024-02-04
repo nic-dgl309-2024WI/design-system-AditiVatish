40e74bd65cc5be96d8439bf4359e68d5508dd494
AditiVatish <144161682+AditiVatish@users.noreply.github.com>
2024-02-04 02:13:01 -0800

    Add JavaScript function to toggle mobile navigation

--- design-system.js
+++ design-system.js
@@ -1,1 +1,8 @@
+ <script>
+        // JavaScript function to toggle mobile navigation
+        function toggleMobileNav() {
+            var mobileNav = document.querySelector('.mobile-nav ul');
+            mobileNav.style.display = (mobileNav.style.display === 'none' || mobileNav.style.display === '') ? 'block' : 'none';
+        }
+    </script>
 
