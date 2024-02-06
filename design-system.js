d59b2860d3caaef72c5a6f05baf46f55f9cc3d20
AditiVatish <144161682+AditiVatish@users.noreply.github.com>
2024-02-06 01:15:25 -0800

    Script to show hide side bar by click on icons

--- design-system.js
+++ design-system.js
@@ -1,8 +1,17 @@
- <script>
-        // JavaScript function to toggle mobile navigation
-        function toggleMobileNav() {
-            var mobileNav = document.querySelector('.mobile-nav ul');
-            mobileNav.style.display = (mobileNav.style.display === 'none' || mobileNav.style.display === '') ? 'block' : 'none';
-        }
-    </script>
-
+function toggleNavbar() {
+    let hamburgerIcon = document.getElementById('ham-open');
+    let closeIcon = document.getElementById('close-icon');
+    let sideNav = document.getElementById('side-navbar');
+    let topNav = document.getElementById('nv-link');
+  
+  
+  
+    hamburgerIcon.classList.toggle("show-icon");
+    closeIcon.classList.toggle("show-icon");
+    sideNav.classList.toggle("show-icon");
+    topNav.classList.toggle("show-icon");
+  
+  
+  
+  }
+  
