82e7ad6a99127fd406632473a36619875e62792e
AditiVatish <144161682+AditiVatish@users.noreply.github.com>
2024-02-26 01:17:58 -0800

    Update design-system.js

--- design-system.js
+++ design-system.js
@@ -7,5 +7,17 @@ function toggleNavbar() {
   hamburgerIcon.classList.toggle("show-icon");
   closeIcon.classList.toggle("show-icon");
   sideNav.classList.toggle("show-icon");
-  topNav.classList.toggle("show-icon");
+
+  // Toggle body overflow to prevent scrolling when the side navbar is open
+  document.body.classList.toggle("no-scroll");
 }
+
+// Close the side navbar when a link is clicked
+document.querySelectorAll('#side-navbar a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.getElementById('ham-open').classList.remove('show-icon');
+    document.getElementById('close-icon').classList.remove('show-icon');
+    document.getElementById('side-navbar').classList.remove('show-icon');
+    document.body.classList.remove('no-scroll');
+  });
+});
