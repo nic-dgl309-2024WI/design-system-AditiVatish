f1d380613b51dfdfa1ca06ee6597f2088f6e3077
AditiVatish <144161682+AditiVatish@users.noreply.github.com>
2024-02-26 01:18:14 -0800

    Update design-system.js

--- design-system.js
+++ design-system.js
@@ -2,7 +2,6 @@ function toggleNavbar() {
   let hamburgerIcon = document.getElementById('ham-open');
   let closeIcon = document.getElementById('close-icon');
   let sideNav = document.getElementById('side-navbar');
-  let topNav = document.getElementById('nv-link');
 
   hamburgerIcon.classList.toggle("show-icon");
   closeIcon.classList.toggle("show-icon");
