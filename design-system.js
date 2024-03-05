57d5da45ee13c6d4ea32cff87264047881197eba
AditiVatish <144161682+AditiVatish@users.noreply.github.com>
2024-03-04 23:39:10 -0800

    Update design-system.js

--- design-system.js
+++ design-system.js
@@ -1,11 +1,11 @@
-function toggleNavbar() {
-  let hamburgerIcon = document.getElementById('ham-open');
-  let closeIcon = document.getElementById('close-icon');
-  let sideNav = document.getElementById('side-navbar');
-  let topNav = document.getElementById('nv-link');
-
-  hamburgerIcon.classList.toggle("show-icon");
-  closeIcon.classList.toggle("show-icon");
-  sideNav.classList.toggle("show-icon");
-  topNav.classList.toggle("show-icon");
+function toggleNav() {
+  var content = document.getElementById("content");
+  var toggleButton = document.querySelector(".toggle");
+  if (content.style.display === "block") {
+    content.style.display = "none";
+    toggleButton.classList.remove("active");
+  } else {
+    content.style.display = "block";
+    toggleButton.classList.add("active");
+  }
 }
