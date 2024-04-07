63228b5e19c7b10f9875683844f3c61783062a85
AditiVatish <144161682+AditiVatish@users.noreply.github.com>
2024-04-07 08:11:48 -0700

    Java Script for Carousel

--- design-system.js
+++ design-system.js
@@ -6,4 +6,25 @@ function toggleNavbar() {
     document.getElementById('nv-link').classList.toggle("show-icon"); 
 
 
-}
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+    const projects = document.querySelectorAll('.products');
+    const totalProjects = projects.length;
+    const carousel = document.querySelector('.carousel-container');
+    
+    document.querySelector('.carousel-button-prev').addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + totalProjects) % totalProjects;
+      updateCarousel();
+    });
+    
+    document.querySelector('.carousel-button-next').addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % totalProjects;
+      updateCarousel();
+    });
+    
+    function updateCarousel() {
+      const projectWidth = projects[0].clientWidth;
+      const newTransform = -currentIndex * (projectWidth + 40); // 40 is the margin-right value of .pcards
+      carousel.style.transform = `translateX(${newTransform}px)`;
+    }
+})
